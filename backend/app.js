e6300a242702ffba16e9b01924347d5d2ee5199e
monkey-on-drugs <kesharwanimanas.college@gmail.com>
2024-02-05 03:00:53 +0530

    12345

--- backend/app.js
+++ backend/app.js
@@ -3,24 +3,24 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const errorMiddleware = require("./middlewares/error.js")
+const errorMiddleware = require("./middlewares/error.js");
 
-app.use(express.json())
+app.use(express.json());
 app.use(
   cors({
     origin: "https://math-quiz-webapp-jopc.vercel.app",
     credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Import routes
-const routes = require("./routes/index.js")
+const routes = require("./routes/index.js");
 
 app.use("/backend-api", routes);
 
 // Middleware for Error Handling
-app.use(errorMiddleware)
-
+app.use(errorMiddleware);
 
 module.exports = app;
